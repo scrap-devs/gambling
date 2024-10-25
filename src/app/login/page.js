92e5b20ffbe68151e4eec8a2ef3fe0b4f7a69ee6
uspilledtea <nguyenhbryan0818@gmail.com>
2024-10-24 21:38:54 -0400

    edit login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -27,8 +27,8 @@ export default function Login() {
   return (
     <section className="w-full h-screen bg-slate-800 flex items-center justify-center">
       <form
-        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
-        border border-solid border-black bg-slate-900 rounded"
+        className="p-6 w-full max-w-[500px] flex flex-col justify-between items-center gap-2 
+        border border-solid border-black bg-slate-900 rounded-xl"
         onSubmit={handleSubmit}>
         {error && <div className="text-white">{error}</div>}
         <h1 className="mb-5 w-full text-2xl text-white font-bold">Sign In</h1>
@@ -46,7 +46,7 @@ export default function Login() {
             className="w-full h-8 border border-solid border-black rounded p-2"
             name="password" />
         </div>
-        <button className="w-full border border-solid border-black rounded">
+        <button className="w-full text-white border border-solid border-white rounded">
           Sign In
         </button>
         <Link
