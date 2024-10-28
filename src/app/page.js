d663f5b777d50bc7f8f0c97ecea1cf16fe5f5467
scrap-devs <lucasung07@gmail.com>
2024-10-28 18:40:08 +0000

    sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -4,46 +4,16 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import "./globals.css";
 import "./page.module.css";
+import Sidenav from "./components/Sidenav";
 
 export default function Home() {
-  const { status } = useSession();
-  const router = useRouter();
-
-  const showSession = () => {
-    if (status === "authenticated") {
-      return (
-        <button
-          className="border border-solid border-black rounded"
-          onClick={() => {
-            signOut({ redirect: false }).then(() => {
-              router.push("/");
-            });
-
-          }}
-        >
-          Sign Out
-        </button>
-      )
-    } else if (status === "loading") {
-      return (
-        <span className="text-[#888] text-sm mt-7">Loading...</span>
-      )
-    } else {
-      return (
-        <Link
-          href="/login"
-          className="border border-solid border-black rounded"
-        >
-          Sign In
-        </Link>
-      )
-    }
-  }
+  
   return (
-      <div className="flex flex-row-reverse w-screen bg-slate-900 h-screen"> 
-            <p className="text-white">{showSession()}</p>
+      <div className="w-screen bg-slate-900 h-screen"> 
             
-        </div>
+            <Sidenav/>
+            
+       </div>
     
   );
 }
