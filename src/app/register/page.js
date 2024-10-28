8823f0f861c938a08534bba49e2ea970c1fc3963
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-10-28 18:27:23 +0000

    updated register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -26,15 +26,15 @@ export default function Register() {
     };
 
     return (
-        <section className="w-full h-screen flex items-center justify-center">
+        <section className="w-full h-screen flex items-center justify-center bg-slate-800">
             <form ref={ref}
                 action={handleSubmit}
                 className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
-            border border-solid border-black bg-white rounded">
+             border-white bg-slate-900 rounded">
                 {error && <div className="">{error}</div>}
-                <h1 className="mb-5 w-full text-2xl font-bold">Register</h1>
+                <h1 className="mb-5 w-full text-white text-2xl font-bold">Register</h1>
 
-                <label className="w-full text-sm">Full Name</label>
+                <label className="w-full text-white text-sm">Full Name</label>
                 <input
                     type="text"
                     placeholder="Full Name"
@@ -42,7 +42,7 @@ export default function Register() {
                     name="name"
                 />
 
-                <label className="w-full text-sm">Email</label>
+                <label className="w-full text-white text-sm">Email</label>
                 <input
                     type="email"
                     placeholder="Email"
@@ -50,7 +50,7 @@ export default function Register() {
                     name="email"
                 />
 
-                <label className="w-full text-sm">Password</label>
+                <label className="w-full text-white text-sm">Password</label>
                 <div className="flex w-full">
                     <input
                         type="password"
@@ -60,13 +60,13 @@ export default function Register() {
                     />
                 </div>
 
-                <button className="w-full border border-solid border-black py-1.5 mt-2.5 rounded
-            transition duration-150 ease hover:bg-black">
+                <button className="w-full border border-solid border-white text-white py-1.5 mt-2.5 rounded
+            transition duration-150 eas">
                     Sign up
                 </button>
 
 
-                <Link href="/login" className="text-sm text-[#888] transition duration-150 ease hover:text-black">
+                <Link href="/login" className="text-sm text-[#888] transition duration-150 ease hover:text-white">
                     Already have an account?
                 </Link>
             </form>
