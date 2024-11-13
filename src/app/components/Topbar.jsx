60aaee008ebd8c6f3b25a1c748060206d6b3d1f4
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-11-13 19:17:47 +0000

    topbar

--- src/app/components/Topbar.jsx
+++ src/app/components/Topbar.jsx
@@ -1,11 +1,27 @@
 'use client'
 
 import React from "react"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Topbar() {
     return (<>
-        <div className="flex w-screen h-12 bg-white">
-            dkkdkdkdk
+        <div className="flex w-full h-12 bg-white justify-between">
+
+            <Link href="/">
+                <Image
+                    className="mt-2 mb-8 h-100 w-100"
+                    src="/images/mainicon.png"
+                    height={32}
+                    width={32}
+                    alt="logo"
+                />
+            </Link>
+            <div>Coin Craze</div>
+            <div>
+            Login
+            </div>
+
         </div>
     </>);
 
