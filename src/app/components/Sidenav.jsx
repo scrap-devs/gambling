1c132138361ed7c6c8055cce39131eb1b7f30d92
uspilledtea <nguyenhbryan0818@gmail.com>
2024-11-16 00:39:05 -0500

    Fixed text on expanded sidebar

--- src/app/components/Sidenav.jsx
+++ src/app/components/Sidenav.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 // Import necessary libraries and components
 import React, { useState, useEffect, useRef } from "react";
@@ -8,7 +8,6 @@ import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoIosHome } from "react-icons/io";
-
 
 // Define the Sidenav component
 export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
@@ -21,7 +20,7 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
   // Effect to add or remove a class to the body element based on sidebar expansion
   useEffect(() => {
     const body = document.querySelector("body");
-    body.classList.toggle("sidebar-expanded", sidebarExpanded)
+    body.classList.toggle("sidebar-expanded", sidebarExpanded);
   }, [sidebarExpanded]);
 
   return (
@@ -29,8 +28,9 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
       {/* Sidebar backdrop (visible on mobile only) */}
       <div
         onClick={() => setSidebarOpen(!sidebarOpen)}
-        className={`fixed inset-0 border-r bg-gray-800 border-gray-200 sm:translate-x-0 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-          }`}
+        className={`fixed inset-0 border-r bg-gray-800 border-gray-200 sm:translate-x-0 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
+          sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
         aria-hidden="true"
       ></div>
 
@@ -38,9 +38,11 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`fixed flex flex-col bg-gray-800 z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar lg:sidebar-expanded:w-20 shrink-0 ${sidebarExpanded ? "w-56" : "w-20"
-          } p-4 transition-all duration-200 ${sidebarOpen ? "translate-x-0" : "-translate-x-72"
-          }`}
+        className={`fixed flex flex-col bg-gray-800 z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar lg:sidebar-expanded:w-20 shrink-0 ${
+          sidebarExpanded ? "w-56" : "w-20"
+        } p-4 transition-all duration-200 ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-72"
+        }`}
       >
         <div className="flex flex-column sm:px-2">
           {/* Expand / collapse button */}
@@ -56,22 +58,21 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
               </button>
             </div>
             <div>
-              <Link href="/">
-                <IoIosHome color="white" size={25} className="inline-block" />
-                <span className={`pl-6 text-white ${sidebarExpanded ? "opacity-100" : "hidden pointer-events-none"}`}>
+              <Link href="/" className="flex">
+                <IoIosHome color="white" size={25} />
+                <span
+                  className={`pl-6 text-white ${
+                    sidebarExpanded
+                      ? "opacity-100"
+                      : "hidden pointer-events-none"
+                  }`}
+                >
                   Home
                 </span>
-
-
-              </Link >
+              </Link>
             </div>
           </div>
         </div>
-
-
-
-
-
       </div>
     </>
   );
