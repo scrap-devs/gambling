dc9347b750846d29a058160a23c17eb155c5d325
uspilledtea <119764073+uspilledtea@users.noreply.github.com>
2024-11-19 18:41:14 +0000

    sidenav update

--- src/app/components/Sidenav.jsx
+++ src/app/components/Sidenav.jsx
@@ -27,9 +27,8 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
       {/* Sidebar backdrop (visible on mobile only) */}
       <div
         onClick={() => setSidebarOpen(!sidebarOpen)}
-        className={`fixed inset-0 border-r bg-gray-800 border-gray-200 sm:translate-x-0 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${
-          sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
-        }`}
+        className={`fixed inset-0 border-r bg-gray-800 border-gray-200 sm:translate-x-0 bg-opacity-30 z-40 lg:hidden lg:z-auto transition-opacity duration-200 ${sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          }`}
         aria-hidden="true"
       ></div>
 
@@ -37,16 +36,14 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`fixed flex flex-col bg-gray-800 z-40 left-0 top-0 md:static md:left-auto md:top-auto md:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar lg:sidebar-expanded:w-20 shrink-0 ${
-          sidebarExpanded ? "w-56" : "w-20"
-        } p-4 transition-all duration-200 ${
-          sidebarOpen ? "translate-x-0" : "-translate-x-72"
-        }`}
+        className={`fixed flex flex-col bg-gray-800 z-40 left-0 top-0 md:static md:left-auto md:top-auto md:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar lg:sidebar-expanded:w-20 shrink-0 ${sidebarExpanded ? "w-56" : "w-20"
+          } p-4 transition-all duration-200 ${sidebarOpen ? "translate-x-0" : "-translate-x-72"
+          }`}
       >
         <div className="flex flex-column sm:px-2">
           {/* Expand / collapse button */}
 
-          <div className="pb-3 justify-end">
+          <div className="pb-3 justify-center">
             <div className="pb-5">
               <button
                 onClick={() => {
@@ -60,19 +57,35 @@ export default function Sidenav({ sidebarOpen, setSidebarOpen }) {
               </button>
             </div>
             {/* Home Button*/}
-            <div>
-              <Link href="/" className="flex">
-                <IoIosHome color="white" size={25} />
-                <span
-                  className={`pl-6 text-white ${
-                    sidebarExpanded
-                      ? "opacity-100"
-                      : "hidden pointer-events-none"
-                  }`}
-                >
-                  Home
-                </span>
-              </Link>
+            <div className="rounded-md bg-gray-700">
+              <div className="p-3">
+                <div className="pb-3">
+                  <Link href="/" className="flex">
+                    <IoIosHome color="white" size={25} />
+                    <span
+                      className={`pl-6 text-white w-32 ${sidebarExpanded
+                          ? "opacity-100"
+                          : "hidden pointer-events-none"
+                        }`}
+                    >
+                      
+                    </span>
+                  </Link>
+                </div>
+                <div>
+                  <Link href="/" className="flex">
+                    <IoIosHome color="white" size={25} />
+                    <span
+                      className={`pl-6 text-white ${sidebarExpanded
+                          ? "opacity-100"
+                          : "hidden pointer-events-none"
+                        }`}
+                    >
+                      Home
+                    </span>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
