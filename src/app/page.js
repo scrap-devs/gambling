d98592c8c2eec0519637b7894bb3917e7d0f7b78
scrap-devs <lucasung07@gmail.com>
2024-11-21 19:39:34 +0000

    page renew

--- src/app/page.js
+++ src/app/page.js
@@ -8,17 +8,26 @@ import Sidenav from "./components/Sidenav";
 
 export default function Home() {
   return (
-      <div className="w-screen h-screen bg-zinc-800">
-          {/*Top Componenet */}
-          <div className="bg-zinc-900 flex flex-row w-screen h-40">
-            <div className="text-white basis-0 grow">alksdjflk;sadjf</div>
-            <div className="text-white basis-0 grow">alksdjflk;sadjf</div>
-          </div>
-        {/*Middle Component */}
-        <div className="bg-dark-black flex flex-row basis-0 grow-1 justify-between">
-            askjdfnhkjasdf
-            </div>
+    <div className="w-screen h-screen bg-zinc-800">
+      {/*Top Componenet */}
+      <div className="bg-zinc-900 flex flex-row w-screen h-1/5">
+        <div className="text-white basis-0 grow">alksdjflk;sadjf</div>
+        <div className="text-white basis-0 grow">alksdjflk;sadjf</div>
       </div>
+      {/*Middle Component */}
+      <div className="bg-dark-black flex flex-row basis-0 grow-1 content-center justify-between h-1/5">
+        askjdfnhkjasdf
+      </div>
+      {/*Leaderboard */}
+      <div className="bg-zinc-700 h-3/5 w p-20 border-2 justify-center">
+        <h1 className="text-white">Leaderboard</h1>
+            <ul>
+              <li>
+                skjapdff
+              </li>
+            </ul>
+          </div>
+        </div>
 
   );
 }
