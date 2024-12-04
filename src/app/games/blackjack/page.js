1b37947e2061dc871894ff15637fc433b111e211
scrap-devs <lucasung07@gmail.com>
2024-12-04 19:19:47 +0000

    game page addition

--- src/app/games/blackjack/page.js
+++ src/app/games/blackjack/page.js
@@ -1,1 +1,5 @@
-export default function Blackjack()
+"use client";
+
+export default function Blackjack() {
+
+}
--- src/app/games/page.js
+++ src/app/games/page.js
@@ -5,12 +5,12 @@ import Link from 'next/link';
 
 export default function Games() {
     return (<>
-    <div>
-        Game selections
-        <div>
-            <Link href="/testGame">
-            testGame
-            </Link>
+    <div className="flex flex-col bg-black w-screen h-screen">
+        <div className="flex h-20 w-screen bg-zinc-800 text-2xl align-middle"> Top Games</div>
+        <div className="flex-row bg-zinc-700 p-2 pt-5 h-screen w-screen">
+            <Link href="" className="text-black rounded-xl border border-blue bg-blue-50 hover:shadow-xl text-xl p-4"> Test Game </Link>
+            <Link href="/games/blackjack" className="text-black rounded-xl border border-blue bg-blue-50 hover:bg-white text-xl p-4"> Blackjack </Link>
+            <Link href="/games/" className="text-black rounded-xl border border-blue bg-blue-50 hover:shadow-xl text-xl p-4">  Plinko </Link>
         </div>
     </div>
     </>)
